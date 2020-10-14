fb79bed2d569f77ea28595f2768190c6489ccd52
Shyaboi <ian.shane.sears@gmail.com>
2020-10-13 17:44:25 -0700

    added pstmn discount logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar"
-import ha from  '../../images/ha.png'
+import logo from  '../../images/logosml.png'
 
 function Nav() {
     return (
@@ -8,7 +8,7 @@ function Nav() {
     <Navbar.Brand href="#home">
       <img
         alt=""
-        src={ha}
+        src={logo}
         width="30"
         height="30"
         className="d-inline-block align-top"
