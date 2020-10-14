775d6b48be2d63a453a833c46925810af88ced2b
Shyaboi <ian.shane.sears@gmail.com>
2020-10-14 11:33:43 -0700

    Updates

---
+++ precache-manifest.ae803d338278ab6cdecdadb9ddf24657.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2ae025e73f10bd618a1a0a5c06619a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35d8a4a4fb36382b8a88",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "306e1c07d1efca52c7e4",
+    "url": "/static/css/main.f63794aa.chunk.css"
+  },
+  {
+    "revision": "35d8a4a4fb36382b8a88",
+    "url": "/static/js/2.f68c9682.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f68c9682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306e1c07d1efca52c7e4",
+    "url": "/static/js/main.cfb53427.chunk.js"
+  },
+  {
+    "revision": "a1560475432a9f2bb5ca",
+    "url": "/static/js/runtime-main.a1eb8c97.js"
+  },
+  {
+    "revision": "98327125b8ce675c2b4b9a6cbcd70733",
+    "url": "/static/media/logosml.98327125.png"
+  }
+]);
