a005cc7422cd10b1cfb2681b57172c7b1e15c530
Shyaboi <ian.shane.sears@gmail.com>
2020-10-15 18:14:40 -0700

    will need to add http/https dropdown and diverting paths on post

--- server/server.js
+++ server/server.js
@@ -19,26 +19,34 @@ app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
-app.use(bodyParser.json());
-
-app.get("/", function (req, res) {
-  res.json({ hi: "thisd is a JSON" });
-});
-// main post route for receiving react data
-app.post("/", function (req, res) {
+  );
+  app.use(bodyParser.json());
+  
+  app.get("/", function (req, res) {
+    res.json({ hi: "thisd is a JSON" });
+  });
+  // main post route for receiving react data
+  app.post("/", function (req, res) {
+    // incoming url to parse
+  const urlToParse = req.body.body.url;
   const meth = req.body.body.picked;
   const bod = req.body.body.body;
-  // http get if the incoming method is a get
+  const url = new URL(urlToParse);
+  const urlPort = url.port;
+  const protocol = url.protocol;
+  const host = url.hostname;
+  const path = url.pathname;
 
+  console.log("proto: " + protocol,"port: " + urlPort,`host: ` + host,"path" + path);
+
+  // http get if the incoming method is a get
   if (meth === "GET") {
-    console.log("gettin")
+    console.log("gettin");
     https
       .get(req.body.body.url, (resp) => {
         let data = "";
         resp.on("data", (chunk) => {
           data += chunk;
-          console.log(data);
         });
         resp.on("end", () => {
           const resJson = JSON.parse(data);
@@ -49,68 +57,59 @@ app.post("/", function (req, res) {
         console.log("Error: " + err.message);
       });
   }
-const urlToParse = req.body.body.url
+  
+  // if post method chosen
   if (meth === "POST") {
-    console.log("postn'")
-    
-    if (bod == "{someExampleJSON:morjson}"){
-      console.log('need a bod bud')
+    console.log("postn'");
+
+    if (bod == "{someExampleJSON:morjson}") {
+      console.log("need a bod bud");
     }
     const postBody = JSON.parse(bod);
 
-    const data = JSON.stringify({
-      postBody
-  });
-
-  const url = new URL(urlToParse);
-  const urlPort = url.port
-  const protocol = url.protocol
-  const host = url.hostname
-  const path = url.pathname
-
-  console.log('proto: ' + protocol,'port: '+urlPort, `host: `+host, 'path'+path)
-// console.log(url.pathname)
-  const options = {
+    // console.log(url.pathname)
+    const options = {
       hostname: host,
-      port:urlPort,
+      port: urlPort,
       path: path,
-      method: 'POST',
+      method: "POST",
       headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': bod.length
-      }
-  };
-  
-  
-  const req = http.request(options, (res) => {
-      let data = '';
+        "Content-Type": "application/json",
+        "Content-Length": bod.length,
+      },
+    };
 
-      console.log('Status Code:', res.statusCode);
-      
-      res.on('data', (chunk) => {
-        data += chunk;
+    const req = https
+      .request(options, (res) => {
+        let data = "";
+
+        console.log("Status Code:", res.statusCode);
+
+        res.on("data", (chunk) => {
+          data += chunk;
+          data = data;
+          console.log("respopnse data: " + data);
+          req.json(data);
+        });
+
+        res.on("end", () => {
+          // console.log('Body: ', JSON.parse(bod));
+        });
+      })
+      .on("error", (err) => {
+        console.log("Error: ", err.message);
       });
-      
-      res.on('end', () => {
-        console.log('Body: ', JSON.parse(bod));
-      });
-      
-    }).on("error", (err) => {
-      console.log("Error: ", err.message);
-    });
-    
+
     req.write(bod);
-  res.json(JSON.parse(bod));
-  req.end();
+    res.json(JSON.parse(bod));
+    req.end();
   }
 });
 
-
-
 app.post("/past", function (req, res) {
-  console.log(req.body)
-  res.send(req.body)
-})
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
