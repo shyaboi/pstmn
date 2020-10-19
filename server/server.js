ef95c145540c97e2ffe128fbb79e7ce72207488c
Shyaboi <ian.shane.sears@gmail.com>
2020-10-18 20:40:01 -0700

    changed file routing

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ app.use(
   })
   );
   app.use(bodyParser.json());
-  app.use(express.static(path.join(__dirname, 'build')))
+  app.use(express.static(path.join(__dirname, '../build')))
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../build', 'index.html'))
   })
   // main post route for receiving react data
   app.post("/", function (req, res) {
