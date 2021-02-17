de5b2673c4e39f344da8f74d9bf0b57901277e1a
Shyaboi <ian.shane.sears@gmail.com>
2021-02-17 08:32:00 -0700

    got rid of entire post/get method and not using back end anymore

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ReactJson from 'react-json-view'
+import ReactJson from "react-json-view";
 
 import {
-    Jumbotron,
+  Jumbotron,
   FormControl,
   InputGroup,
   Form,
@@ -23,9 +23,9 @@ class Main extends React.Component {
     this.state = {
       picked: "Method",
       body: '{"someExampleJSON":"morjson"}',
-      responseType:"Example JSON",
+      responseType: "Example JSON",
       response: "{example:JSON}",
-      url: "https://openflags.net/api/usa/california"
+      url: "https://openflags.net/api/usa/california",
     };
   }
   handleClick(positionClicked) {
@@ -33,62 +33,33 @@ class Main extends React.Component {
     this.setState({ picked: positionClicked });
   }
 
-urlChange = (e) => {
-    this.setState({url: e.target.value});
+  urlChange = (e) => {
+    this.setState({ url: e.target.value });
     // console.log(this.state.url)
- }
- bodyChange = (e) => {
-  this.setState({body: e.target.value});
-  console.log(this.state.body)
-}
+  };
+  bodyChange = (e) => {
+    this.setState({ body: e.target.value });
+    console.log(this.state.body);
+  };
+  postData = async () => {
+    let states = await {
+      meth: this.state.picked,
+      body: this.state.body,
+      url: this.state.url,
+    };
 
-  postData = ()=> {
-    console.time('init')
-    // Example POST method implementation:
-    this.setState({ body: this.state.body });
-    // const bod = this.state.body
-    // console.log(JSON.stringify(bod))
-    const meth = this.state.picked
-    console.log(meth)
-    async function postData(url = '', data = {}) {
-      // Default options are marked with *
-      const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'omit', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    console.log(states);
+    if (states.meth === "GET") {
+      const fetchy = async (address) => {
+        let res = await fetch(address);
+        let data = await res.json();
+        return data;
+      };
+      fetchy(states.url).then(async (data) => {
+        console.log(data)
       });
-      return response.json(); // parses JSON response into native JavaScript objects
     }
-
-  
-    
-    postData('http://localhost:4444', { body: this.state })
-    .then(data => {
-      const dataType = typeof(data)
-      if(typeof(data)==='string'){
-        var strang = JSON.stringify(data)
-        this.setState({ response: strang });
-      }
-      if(typeof(data)==='object'){
-      var strangJSON = <ReactJson src={data} />
-      this.setState({ response: strangJSON });
-
-      }
-      this.setState({ responseType: dataType });
-      console.timeEnd('init')
-
-      console.log(data);
-    });
-  }
-
+  };
 
   render() {
     return (
@@ -136,13 +107,20 @@ urlChange = (e) => {
                   id="basic-url"
                   aria-describedby="basic-addon3"
                   placeholder="Example: https://openflags.net/api/json/ISO3166/US-OR"
-                  defaultValue=''
-                  onChange={this.urlChange} 
+                  defaultValue=""
+                  onChange={this.urlChange}
                 />
               </InputGroup>
             </Col>
             <Col md={1}>
-              <Button type="submit" value="Submit" size="lg" onClick={this.postData} >Submit</Button>
+              <Button
+                type="submit"
+                value="Submit"
+                size="lg"
+                onClick={this.postData}
+              >
+                Submit
+              </Button>
             </Col>
           </Row>
         </Container>
@@ -155,8 +133,7 @@ urlChange = (e) => {
               as="textarea"
               rows="3"
               placeholder={this.state.body}
-              onChange={this.bodyChange} 
-
+              onChange={this.bodyChange}
             />
           </Form.Group>
         </Container>
@@ -164,15 +141,11 @@ urlChange = (e) => {
 
         <Container>
           <div>
-            <Jumbotron fluid id='jumbo'>
+            <Jumbotron fluid id="jumbo">
               <Container>
-            <h1>Response From Server</h1>
-            <h2>
-              {this.state.responseType}
-            </h2>
-                <Container>
-                  {this.state.response}
-                </Container>
+                <h1>Response From Server</h1>
+                <h2>{this.state.responseType}</h2>
+                <Container>{this.state.response}</Container>
               </Container>
             </Jumbotron>
           </div>
